66d8044a52649dbd4d085acff17cd61f42c6fe09
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-02 16:16:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count(board)
   count = 0
   board.each do |count_of_plays|
-    if #{count_of_plays}=="X" || #{count_of_plays} == "O"
-      count+=1
+    if #{count_of_plays} =="X" || #{count_of_plays} == "O"
+      count+=1 
     end
   end
 end
